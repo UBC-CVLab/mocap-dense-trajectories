3b92c27bd1ab4c83daa618c1a5b0c1fe36d7a9aa
jltmtz <jltmtzc@gmail.com>
2014-03-11 14:40:44 -0700

    Vectorizing the conversion of trajectory features to physical.

--- mocap-trajectories/physical_traj2traj_features.m
+++ mocap-trajectories/physical_traj2traj_features.m
@@ -1,6 +1,6 @@
 function [ftraj, traj_end] = physical_traj2traj_features(ptraj, frameNo, len_traj)
-% PTRAJ2TRAJ_FEATURES Vectorized conversion of physical trajectory points
-% to traj features (as defined by Wang et al.[1]).
+% PHYSICAL_TRAJ2TRAJ_FEATURES Vectorized conversion of physical trajectory
+% points to traj features (as defined by Wang et al.[1]).
 %
 % Input
 %   ptraj    : ntraj x len_traj*2 Double. 2D point locations of the
@@ -15,30 +15,52 @@ function [ftraj, traj_end] = physical_traj2traj_features(ptraj, frameNo, len_tra
 %
 % [1] Wang, H., & Klaser, A. (2011). Action recognition by dense
 % trajectories. CVPR'11.
+%
 % ---
 % Julieta & Ankur
 
+% If the passed trajectories are empty, return empty arrays.
 if isempty(ptraj),
     traj_end = [];
     ftraj    = [];
     return
 end
+
+% We have 7 entries for metadata.
 NUM_METADATA_ROWS = 7;
+
+% Get the number of passed trajectories.
 [ntraj, ~] = size( ptraj );
 
+% Create space for the output.
 ftraj    = zeros(ntraj, NUM_METADATA_ROWS + 2*len_traj);
-traj_end = ptraj(:, end-1:end); % Save the 2D location where traj terminates.
 
-diff  = ptraj(:, 1:end-2) - ptraj(:, 3:end);
+% Save the 2D location where traj terminates (maybe useful for geometry).
+traj_end = ptraj(:, end-1:end); 
 
-lenT = sum( sqrt(diff(:, 1:2:end).^2+diff(:, 2:2:end).^2), 2 );
-xy = diff./repmat( lenT, [1, size(diff, 2)] );
+% Get necessary info for trajectory features.
+diff     = ptraj(:, 1:end-2) - ptraj(:, 3:end);
 
-ftraj(:, 1)   = repmat(frameNo, [ntraj, 1]);
-ftraj(:, 2:3) = [mean(ptraj(:, 1:2:end), 2) mean(ptraj(:, 2:2:end), 2)];
-ftraj(:, 4:5) = [std(ptraj(:, 1:2:end), 0, 2) std(ptraj(:, 2:2:end), 0, 2)];
-ftraj(:, 6) = lenT;
-ftraj(:, 7) = -1; %% we lose this information
+lenT     = sum( sqrt(diff(:, 1:2:end).^2+diff(:, 2:2:end).^2), 2 );
+xy       = diff ./ repmat( lenT, [1, size(diff, 2)] );
+
+
+% Add metadata.
+% frameNum:     The trajectory ends on this frame
+ftraj(:, 1)       = repmat(frameNo, [ntraj, 1]);
+% mean_x:       The mean value of the x coordinates of the trajectory
+% mean_y:       The mean value of the y coordinates of the trajectory
+ftraj(:, 2:3)     = [mean(ptraj(:, 1:2:end), 2) mean(ptraj(:, 2:2:end), 2)];
+% var_x:        The variance of the x coordinates of the trajectory
+% var_y:        The variance of the y coordinates of the trajectory
+ftraj(:, 4:5)     = [std(ptraj(:, 1:2:end), 0, 2) std(ptraj(:, 2:2:end), 0, 2)];
+% length:       The length of the trajectory
+ftraj(:, 6)       = lenT;
+% scale:        This information is lost due to ortographic projection. Set to -1.
+ftraj(:, 7)       = -1;
+
+% Actually add the trajectory data.
 ftraj(:, 8:2:end) = xy(:, end-1:-2:1);
 ftraj(:, 9:2:end) = xy(:, end:-2:1);
+
 end
--- mocap-trajectories/traj_features2physical_traj.m
+++ mocap-trajectories/traj_features2physical_traj.m
@@ -1,80 +1,77 @@
 function ptraj = traj_features2physical_traj(trajFeatures, numPoints)
-%TRAJ_FEATURES2PHYSICAL_TRAJ Converts trajectory feature to physical traj
+% TRAJ_FEATURES2PHYSICAL_TRAJ Converts trajectory features to physical.
 % Input
-%    trajFeatures: n-by-(6 + 2*numPoints) matrix. Each row is a feature
-%       trajectory formated in the following order.
-%       1:2 - mean of xy trajectories in normalized feature coordinates,
-%       3:4 - variance of xy trajectories in normalized feature
-%             coordinates.
-%       5   - length of traj.
-%       6   - scale at which it was calculated.
-%       7:7+2*numPoints-1 - xy coordinates in normalized feature
-%             coordinates
+%   trajFeatures: n-by-(7 + 2*numPoints) matrix. Each row is a feature
+%       trajectory formated in the following order :
+%           1:2   : Mean of xy trajectories in normalized feature coordinates.
+%           3:4   : Variance of xy trajectories in normalized feature coordinates.
+%           5     : Length of the trajectory.
+%           6     : Scale at which it was calculated.
+%           8:end : XY coordinates in normalized feature coordinates.
 %
-%    numPoints: Integer. Optional. number of points in the trajectory.
+%   numPoints: Integer. Optional. number of points in the trajectory.
 %               Defaults to 15.
 %
 % Output
-%    ptraj - Physical trajectories. I.e. a series of XY in image coordinates
+%    ptraj : n-by-(numPoints*2) matrix of physical trajectories. Each row
+%            is a series of XY points in image coordinates that make up a
+%            physical trajectory.
 %
 % --
-% Ankur
+% Julieta & Ankur
 
-
-%% TODO Vectorize!
-
-%% Defaults.
+% Defaults.
 DEFAULT_NUM_POINTS = 15;
 if nargin <2 
     numPoints = DEFAULT_NUM_POINTS;
 end
 
+% If the passed trajectories are empty, return an empty array.
 if isempty(trajFeatures),
     ptraj = [];
     return;
 end
 
-%% Constants.
+% Constant.
 XY_BEGIN_IDX = 7;
 
-xIndices = XY_BEGIN_IDX :  2 : XY_BEGIN_IDX + 2 * numPoints - 1;
+% Indices to slice x and y entries.
+xIndices = XY_BEGIN_IDX   : 2 : XY_BEGIN_IDX + 2 * numPoints - 1;
 yIndices = XY_BEGIN_IDX+1 : 2 : XY_BEGIN_IDX + 2 * numPoints - 1;
-numTraj = size(trajFeatures, 1);
-ptraj = zeros(numTraj, 2*(numPoints+1));
-    
-if size(trajFeatures, 2) ~= XY_BEGIN_IDX + 2 * numPoints -1
-    error('Please check the length of the input: trajFeature');
-end
-trajMean =  trajFeatures(:, 1:2);      
-xTraj = trajFeatures(:, xIndices);
-yTraj = trajFeatures(:, yIndices);
-trajLen = trajFeatures(:, 5);
+% Get the number of passes trajectories.
+numTraj  = size(trajFeatures, 1);
+% And create space for the output.
+ptraj    = zeros(numTraj, 2*(numPoints+1));
 
-for i = 1:numTraj
-    orderedArr = zeros(1, 2*(numPoints+1));
-    xym  = trajMean(i, :);             % Trajectory mean in image coordiates.
-    % Vm      = trajFeature(3:4);      % Trajectory variance.
-    lent    = trajLen(i);              % Absolute length of trajectory in image space.
-    % scale   = trajFeature(6);        % Scale (?) of the spatial pyramid, I guess.
-    px = xTraj(i, :);
-    py = yTraj(i, :);
-    
-    X = zeros(numPoints+1, 1);
-    Y = zeros(numPoints+1, 1);
-    
-    % Multiply with length.
-    for col_i=2:numPoints+1
-        X(col_i) = X(col_i-1) + px(col_i-1) * lent; %
-        Y(col_i) = Y(col_i-1) + py(col_i-1) * lent; %
-    end
-    % Add the differences to get real trajectory.
-    lm = [mean(X), mean(Y)];
-    Xm = xym - lm;
-    Xarr = (Xm(1)+X(numPoints+1:-1:1));
-    Yarr = (Xm(2)+Y(numPoints+1:-1:1));    
-    for pti = 1:(numPoints+1)
-        orderedArr(2*pti-1) = Xarr(pti);
-        orderedArr(2*pti) = Yarr(pti);       
-    end
-    ptraj(i, :) = orderedArr;        
+if size(trajFeatures, 2) ~= XY_BEGIN_IDX + 2 * numPoints -1
+    error('Check the length of the input trajectories.');
+end
+
+trajMean = trajFeatures(:, 1:2);      
+xTraj    = trajFeatures(:, xIndices);
+yTraj    = trajFeatures(:, yIndices);
+trajLen  = trajFeatures(:, 5);
+
+X = zeros( numTraj, numPoints + 1 );
+Y = zeros( numTraj, numPoints + 1 );
+
+% Multiply times the length.
+for col_i=2:numPoints+1,
+    X(:, col_i) = X(:, col_i-1) + xTraj(:, col_i-1) .* trajLen;
+    Y(:, col_i) = Y(:, col_i-1) + yTraj(:, col_i-1) .* trajLen;
+end
+
+lm = [mean(X, 2), mean(Y, 2)];
+Xm = trajMean - lm;
+
+% Add the mean to the trajectories.
+Xm1 = Xm(:, 1);
+Xm2 = Xm(:, 2);
+
+Xarr = ( Xm1(:, ones(1, numPoints+1)) + X(:, numPoints+1:-1:1) );
+Yarr = ( Xm2(:, ones(1, numPoints+1)) + Y(:, numPoints+1:-1:1) );
+
+for pti = 1:(numPoints+1)
+    ptraj(:,  2*pti-1 ) = Xarr(:,  pti );
+    ptraj(:,  2*pti   ) = Yarr(:,  pti );       
 end
