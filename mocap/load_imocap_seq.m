0e782f82c1f4ebb2e5534d6b6395bf0d60591fd9
Ankur Gupta <ankgupta@dagama.cs.ubc.ca>
2015-12-02 15:14:51 -0800

    Now reads all the joints from the BVH file.

--- mocap/load_imocap_seq.m
+++ mocap/load_imocap_seq.m
@@ -1,9 +1,9 @@
 function [imocap, mocap] = load_imocap_seq( subject_sequence, base_path, target_fps)
 % LOAD_IMOCAP_SEQ Loads the mocap sequence and corresponding metadata.
-% 
+%
 %  Input
-%   subject_sequence : e.g 01_01 02_01
-%   base_path        : String. The folder where the data is.
+%   subject_sequence : e.g 01_01 02_01 or full path in case second argument empty.
+%   base_path        : String. The folder where the data is. [Optional]
 %   target_fps       : Optional Integer. default is 24.
 %
 %  Output
@@ -13,7 +13,7 @@ function [imocap, mocap] = load_imocap_seq( subject_sequence, base_path, target_
 %                     bones: Map returned by CONSTRUCT_BONE_SURFACES.
 %                     xyz  : 1xnum_bones cell array of 3D locations for
 %                             each joint.
-%    mocap      : Original mocap struct returned by LOADBVH. 
+%    mocap      : Original mocap struct returned by LOADBVH.
 %
 % --
 % Alireza & Ankur
@@ -23,9 +23,14 @@ if nargin < 3,
 end
 
 % Load data from bvh file.
-[mocap, time]  = loadbvh([base_path, subject_sequence]);
+if isempty(base_path)
+    % Assume the first argument is the full path.
+    [mocap, time]  = loadbvh(subject_sequence);
+else
+    [mocap, time]  = loadbvh([base_path, subject_sequence]);
+end
 mocap_fps      = 1/(time(2)-time(1));
-  
+
 [targets, name_ind_map]  = get_imocap_targets();
 
 skip      = round(mocap_fps / target_fps);
@@ -36,10 +41,10 @@ trmocap   = cell(1, numel(targets) - 1);
 for joint=1:numel(mocap)
     joint_data = mocap(joint);
     ind = get_imocap_joint_index(joint_data.name, name_ind_map);
-    if ind ~= -1,
+    if ~isempty(joint_data.trans)
         mocap_transformation = joint_data.trans(1:3, :, 1:skip:end);
-        trmocap{1, ind} = squeeze( num2cell( mocap_transformation, [1, 2]) );        
-    end
+        trmocap{1, ind} = squeeze( num2cell( mocap_transformation, [1, 2]) );
+    end    
 end
 
 imocap.trans = trmocap;
